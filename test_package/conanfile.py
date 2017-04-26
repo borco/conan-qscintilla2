26d61a6ed504b29cc0ea35b0e30ed3d6e928daec
Ioan Calin <iborco@gmail.com>
2017-04-26 18:54:54 +0300

    move test on stable channel

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile, CMake
 import os
 
-channel = os.getenv("CONAN_CHANNEL", "testing")
+channel = os.getenv("CONAN_CHANNEL", "stable")
 username = os.getenv("CONAN_USERNAME", "iborco")
 
 class QscintillaTestConan(ConanFile):
